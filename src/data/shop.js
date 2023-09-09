4933d616a35cb9b89b13b3540b642fe57f69dc41
gdeananthap <gdeanantha33@gmail.com>
2023-09-10 01:56:12 +0700

    feat: add shop page

---
+++ src/data/shop.js
@@ -0,0 +1,24 @@
+export const shopList = [
+  {
+    id: 0,
+    icon: {
+      large: 'tokopedia.png',
+      small: 'tokopediaIcon.png',
+    },
+    tag: {
+      text: 'Beauty Lovers',
+      color: 'green.90'
+    },
+  },
+  {
+    id: 1,
+    icon: {
+      large: 'shopee.png',
+      small: 'shopeeIcon.png',
+    },
+    tag: {
+      text: 'Beauty Lovers',
+      color: 'orange.90'
+    },
+  },
+]
