caa914ff17b4199611e578a7d08b5b3f81ae0ca1
gdeananthap <gdeanantha33@gmail.com>
2023-09-09 19:12:35 +0700

    feat: setup routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css';
 import theme from './theme';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Error from './pages/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+          {/* <Route path="/" element={<Layout />}> */}
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
